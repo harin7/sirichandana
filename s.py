ec41d1735b398cf06615fb33ec266970c5289b45
sirichandana045 <40882441+sirichandana045@users.noreply.github.com>
2018-07-06 12:35:26 +0530

    Create s.py

---
+++ s.py
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i;
+scanf("%d",&n);
+if(i>0)
+{
+printf("The no is positive");
+}
+else if(i<0)
+{
+printf("The no is negative");
+}
+else
+{
+printf("The no is equal to 0");
+}
+return 0;
+}
