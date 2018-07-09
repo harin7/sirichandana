7bb818b32a137b623ae761c21bf01b51921bbbd1
sirichandana045 <40882441+sirichandana045@users.noreply.github.com>
2018-07-09 11:12:44 +0530

    Create siriii.cpp

---
+++ siriii.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char b;
+cout<<"enter the character";
+cin>>b;
+if(b>='a'||b>='A')
+   {
+   if(b<='z'||b<='Z')
+    cout<<" alphabet";
+   }
+else
+cout<<" not a alphabet";
+}
