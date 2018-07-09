12a575f1169165eea6ebd8d1d583f547f330e0be
sirichandana045 <40882441+sirichandana045@users.noreply.github.com>
2018-07-09 10:15:55 +0530

    Create chandu.py

---
+++ chandu.py
@@ -0,0 +1,12 @@
+
+x =  (raw_input(" enter a letter " ))
+
+if x in ['a','e','i','o','u']:
+    
+     print "letter is vowel"
+
+elif x in ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']:
+     print "letter is consonent "
+
+else:
+      print"invalid input"
