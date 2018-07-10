8a30276a4fda9056b7373ecca5db6866951d6d73
sirichandana045 <40882441+sirichandana045@users.noreply.github.com>
2018-07-10 09:19:59 +0530

    Create sss.py

---
+++ sss.py
@@ -0,0 +1,60 @@
+N=input()
+K=input()
+x=0
+if N.isalpha() or K.isalpha():
+    print("Invalid")
+else:
+    N=int(N)
+    K=int(K)
+    array=[int(input()) for _ in range(N)]
+    for i in range(K):
+        x=x+array.
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        00
+        
+        0[i]
+    print(sum)
