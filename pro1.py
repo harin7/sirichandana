b6fe2ffdb9fdb61af8811fad98365a0e07ecc953
sirichandana045 <40882441+sirichandana045@users.noreply.github.com>
2018-07-16 10:57:38 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -4,11 +4,11 @@ u = []
 for i in range(0,v):
     w=input()
     u.append(w)
-prefix_lenght = lenght(u[0])
+prefix_len = len(u[0])
 for x in u[1 : ]:
-    prefix_lenght = min(prefix_lenght, lenght(x))
-    while not x.startswith(u[0][ : prefix_lenght]):
-        prefix_lenght -= 1
+    prefix_len = min(prefix_len, len(x))
+    while not x.startswith(u[0][ : prefix_len]):
+        prefix_len-= 1
 
-prefix = u[0][ : prefix_lenght]
+prefix = u[0][ : prefix_len]
 print (prefix)
